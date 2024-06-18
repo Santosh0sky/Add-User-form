b2d2bea094affc29d4ab8f4b0e0a5ff7d2d37264
Santosh0sky <santosh.kr.yd.sky@gmail.com>
2024-06-18 13:38:42 +0530

    added card

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from '../UI/Card';
 
 const AddUser = () => {
 
@@ -7,13 +8,21 @@ const AddUser = () => {
     }
 
     return(
+        <Card className='input'>
         <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input type ="text" id = "username"/>
-            <label htmlFor='age'>Age</label>
-            <input type ="number" id = "age"/>
+            <div>
+                <div>
+                    <label htmlFor='username'>Username</label>
+                    <input type ="text" id = "username"/>
+                </div>
+                <div>
+                    <label htmlFor='age'>Age</label>
+                    <input type ="number" id = "age"/>
+                </div>
             <button type="submit">Add User</button>
+            </div>
         </form>
+        </Card>
     );
 
 }
